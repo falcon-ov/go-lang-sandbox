107badc30ad059afb33882fc0d98e12ce4cd17b8
falcon-ov <vinifon4he@gmail.com>
2025-07-15 22:46:08 +0300

    feat: add 2 problems

---
+++ go_lang_book/variables_1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// Используя пример программы выше напишите программу,
+// переводящую температуру из градусов Фаренгейта в градусы Цельсия.
+// (C = (F - 32) * 5/9)
+
+func main() {
+	var input float32
+	fmt.Print("Введите температуру в Фаренгетах: ")
+	fmt.Scanf("%f", &input)
+	celsiusValue := (input - 32) * 5 / 9
+	fmt.Println("В градусах Цельсия ", celsiusValue)
+}
